9b674a0957839716586aa8097db1aacbbc3d1b71
Tushar Rawat <tusharrawat52@gmail.com>
2023-12-03 04:02:05 +0530

    hey4

--- components/about.tsx
+++ components/about.tsx
@@ -6,12 +6,12 @@ import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
-  const { ref } = useSectionInView("About");
+  const { ref } = useSectionInView("About Me");
 
   return (
     <motion.section
       ref={ref}
-      className="mb-28 mt-20 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className="mb-28 max-w-[45rem] text-center mt-20 leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
@@ -19,31 +19,25 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+        I am living in Delhi for past 3 years, Now in{" "}
+        <span className="font-medium">Janakpuri</span>, I decided to pursue my
+        passion for programming. I enrolled in a Engineering College,{" "}
+        <span className="font-medium">I can say that I am an Self-Taught Programmer.</span> {""}
+        <span className="italic">In Personal Life , I am funny and Humor is my thing,</span> {""}
+        I feel like one Should Live his/her life end to end and should not leave any room for regret !!  <span className="underline"></span> 
+        {" "}
+        <span className="font-medium flex flex-col">
+          Cool , Funny , Very Spontaneous , adventurous {" "}
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+       {" "}
+        <span className="font-medium flex flex-col text-red-500 ">  
+         ( Sorry I used your photograph here without your permission )
+        </span>
+        
+     
       </p>
 
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p>
+      
     </motion.section>
   );
 }
