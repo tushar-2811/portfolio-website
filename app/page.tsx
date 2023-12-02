aae9441f02b5806cf06c7e5f73f51cab72b4104a
Tushar Rawat <tusharrawat52@gmail.com>
2023-12-03 03:59:17 +0530

    hey2

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import About from "@/components/about";
-import Contact from "@/components/contact";
 import Intro from "@/components/intro";
-import SectionDivider from "@/components/section-divider";
-import Skills from "@/components/skills";
+
 
 export default function Home() {
   return (
